e9d2044c4db56bbcb920c3f617f7a9d89ced4a46
Sushanth <rajasushanth@outlook.com>
2016-12-19 17:28:35 +0530

    Added few files

---
+++ src/main/java/com/test/model/PersonVO.java
@@ -0,0 +1,14 @@
+package com.test.model;
+
+public class PersonVO {
+	
+	private String name;
+	private String project;
+	private String role;
+	@Override
+	public String toString() {
+		return "PersonVO [name=" + name + ", project=" + project + ", role=" + role + "]";
+	}
+	
+	
+}
